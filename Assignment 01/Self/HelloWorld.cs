ea5fbee4a72899ff0303554d9b9de6335efaa0d6
Ali Sweilem <alisweilem2004@gmail.com>
2026-08-13 14:36:43 +0200

    first commit

---
+++ Assignment 01/Self/HelloWorld.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Depi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
