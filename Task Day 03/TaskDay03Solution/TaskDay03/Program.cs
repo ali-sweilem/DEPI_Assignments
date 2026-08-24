08a2a2508a8683c6504c5b37d644aef381b55590
Ali Sweilem <alisweilem2004@gmail.com>
2026-08-24 13:46:09 +0200

    Upload Task 03

---
+++ Task Day 03/TaskDay03Solution/TaskDay03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TaskDay03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int conv1 = int.Parse(input);
+            Console.WriteLine(conv1);
+
+            int conv2 = Convert.ToInt32(input);
+            Console.WriteLine(conv2);
+
+        }
+    }
+}
