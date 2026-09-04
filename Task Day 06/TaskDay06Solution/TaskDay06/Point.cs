946887a57e151aed4b61125ca2023f9d64345fd4
Ali Sweilem <alisweilem2004@gmail.com>
2026-09-04 14:59:33 +0200

    Update code file

--- Task Day 06/TaskDay06Solution/TaskDay06/Point.cs
+++ Task Day 06/TaskDay06Solution/TaskDay06/Point.cs
@@ -8,28 +8,57 @@ namespace TaskDay06
 {
     internal class Point
     {
-        private int X;
-        private int Y;
+        #region Problem1
+        //private int X;
+        //private int Y;
 
-        // Default Ctor
-        public Point()
-        {
-            X = 0;
-            Y = 0;
-        }
+        //// Default Ctor
+        //public Point()
+        //{
+        //    X = 0;
+        //    Y = 0;
+        //}
 
-        // Parameterized Ctor
-        public Point(int x, int y)
-        {
-            X = x;
-            Y = y;
-        }
+        //// Parameterized Ctor
+        //public Point(int x, int y)
+        //{
+        //    X = x;
+        //    Y = y;
+        //}
 
-        // Override ToString()
-        public override string ToString()
-        {
-            return $"({X}, {Y})";
-        }
+        //// Override ToString()
+        //public override string ToString()
+        //{
+        //    return $"({X}, {Y})";
+        //}
+        #endregion
+
+        #region Problem4
+
+        //private int X;
+        //private int Y;
+
+        //// Constructor 1:
+        //public Point(int x)
+        //{
+        //    X = x;
+        //    Y = 0;
+        //}
+        //// Constructor 2:
+        //public Point(int x, int y)
+        //{
+        //    X = x;
+        //    Y = y;
+        //}
+
+        #endregion
+
+        #region Problem5
+        //public override string ToString()
+        //{
+        //    return $"({X}, {Y})";
+        //}
+        #endregion
 
     }
 }
