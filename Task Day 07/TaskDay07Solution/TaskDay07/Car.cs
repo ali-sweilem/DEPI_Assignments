1b17a7b59b0eb5ee339ecff31ef1abfbd648968b
Ali Sweilem <alisweilem2004@gmail.com>
2026-09-07 16:36:18 +0200

    Upload code file

---
+++ Task Day 07/TaskDay07Solution/TaskDay07/Car.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskDay07
+{
+    internal class Car
+    {
+        private int id;
+        private string brand;
+        private decimal price;
+
+        // 1. Defualt Constructor
+        public Car()
+        {
+            id = 0;
+            brand = "Unknown";
+            price = 0;
+        }
+
+        // 2. Constructor with 'Id' parameter
+
+        public Car(int id)
+        {
+            this.id = id;
+        }
+
+        // 3. Constructor with two parameters (Id, Brand)
+
+        public Car(int id, string brand)
+        {
+            this.id = id;
+            this.brand = brand;
+        }
+
+        // 3. Constructor with all three parameters.
+
+        public Car(int id, string brand, decimal price)
+        {
+            this.id = id;
+            this.brand = brand;
+            this.price = price;
+        }
+
+        public int Id { get; }
+        public int Brand { get; }
+        public int Price { get; }
+    }
+}
---
+++ Task Day 07/TaskDay07Solution/TaskDay07/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TaskDay07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Default ctor
+            Car c1 = new Car();
+
+            // Ctor with one Parameter(Id)
+            Car c2 = new Car(1);
+
+            // Ctor with two Parameter(Id, Brand)
+            Car c3 = new Car(2, "BMW");
+
+            Car c4 = new Car(3, "TOYOTA", 1500000);
+
+            Console.WriteLine($"{c1.Id} - {c1.Brand} - {c1.Price}");
+            Console.WriteLine($"{c2.Id} - {c2.Brand} - {c2.Price}");
+            Console.WriteLine($"{c3.Id} - {c3.Brand} - {c3.Price}");
+            Console.WriteLine($"{c4.Id} - {c4.Brand} - {c4.Price}");
+
+        }
+    }
+}
