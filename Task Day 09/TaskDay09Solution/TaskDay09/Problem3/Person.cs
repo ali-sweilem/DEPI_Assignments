63ffa2484c1431add11bed7394294d7a57859585
Ali Sweilem <alisweilem2004@gmail.com>
2026-09-16 17:08:43 +0200

    Update code file

---
+++ Task Day 09/TaskDay09Solution/TaskDay09/Problem3/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskDay09.Problem3
+{
+    internal class Person
+    {
+        public string Department { get; set; }
+
+        public Person() : this("unknown")
+        {
+            
+        }
+
+        public Person(string dep)
+        {
+            Department = dep;
+        }
+
+        public override string ToString()
+        {
+            return $"The department is: {Department}";
+        }
+    }
+}
--- Task Day 09/TaskDay09Solution/TaskDay09/Program.cs
+++ Task Day 09/TaskDay09Solution/TaskDay09/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TaskDay09.Problem3;
 
 namespace TaskDay09
 {
@@ -6,10 +7,26 @@ namespace TaskDay09
     {
         static void Main(string[] args)
         {
-            foreach (Weekdays day in Enum.GetValues(typeof(Weekdays)))
-            {
-                Console.WriteLine($"{day} = {(int)day}");
-            }
+            #region Problem1
+            //foreach (Weekdays day in Enum.GetValues(typeof(Weekdays)))  // Old style
+            //{
+            //    Console.WriteLine($"{day} = {(int)day}");
+            //}
+            #endregion
+
+            #region Problem2
+            //foreach (Grades grade in Enum.GetValues<Grades>())  // Style Generic
+            //{
+            //    Console.WriteLine($"{grade} = {(short)grade}");
+            //}
+            #endregion
+
+            Person p1 = new Person();
+            Person p2 = new Person("CS");
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+
         }
 
         public enum Weekdays
@@ -20,5 +37,16 @@ namespace TaskDay09
             Thursday, 
             Friday
         }
+
+        public enum Grades : short
+        {
+            A = 4,
+            B = 3,
+            C = 2,
+            D = 1,
+            F = -1
+        }
+
+
     }
 }
