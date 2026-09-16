4bad4840a928b43d92c3deee78934eb786b5ce7b
Ali Sweilem <alisweilem2004@gmail.com>
2026-09-16 16:53:58 +0200

    Upload code file

---
+++ Task Day 09/TaskDay09Solution/TaskDay09/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TaskDay09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (Weekdays day in Enum.GetValues(typeof(Weekdays)))
+            {
+                Console.WriteLine($"{day} = {(int)day}");
+            }
+        }
+
+        public enum Weekdays
+        {
+            Monday = 1,
+            Tuesday, 
+            Wednesday,
+            Thursday, 
+            Friday
+        }
+    }
+}
