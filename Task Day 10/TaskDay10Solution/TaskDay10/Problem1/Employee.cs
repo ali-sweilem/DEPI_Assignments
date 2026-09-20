aa5e0d2c702b6a9b9b848222ffa3842ee57aa477
Ali Sweilem <alisweilem2004@gmail.com>
2026-09-20 18:12:46 +0200

    Update code file

---
+++ Task Day 10/TaskDay10Solution/TaskDay10/Problem1/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskDay10.Problem1
+{
+    internal class Employee : IComparable<Employee>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+
+        public Employee(int id, string name, decimal salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+        public int CompareTo(Employee other)
+        {
+            return Id.CompareTo(other.Id);
+        }
+
+        public override string ToString()
+        {
+            return $"Id = {Id}, Name = {Name}, Salary = {Salary}";
+        }
+    }
+}
--- Task Day 10/TaskDay10Solution/TaskDay10/Program.cs
+++ Task Day 10/TaskDay10Solution/TaskDay10/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using TaskDay10.Problem1;
 
 namespace TaskDay10
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Employee[] emp =
+            {
+                new Employee (3, "Ali", 500),
+                new Employee (1, "Moo", 1500),
+                new Employee (2, "Abdo", 350)
+            };
+
+            SortingAlgorithm<Employee>.Sort (emp);
+            foreach (Employee item in emp)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
