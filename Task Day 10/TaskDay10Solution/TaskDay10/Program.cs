dc9ee8fa2fc3efbcfa56d592630bdcfa97d4f8c7
Ali Sweilem <alisweilem2004@gmail.com>
2026-09-20 17:15:02 +0200

    Upload Task Day 10

---
+++ Task Day 10/TaskDay10Solution/TaskDay10/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TaskDay10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
