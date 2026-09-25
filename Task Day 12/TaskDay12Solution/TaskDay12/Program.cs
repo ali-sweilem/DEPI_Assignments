4bb75e7b7fe3dd6d5f018f789c92b941fd9bc3b9
Ali Sweilem <alisweilem2004@gmail.com>
2026-09-25 16:40:59 +0200

    Upload code file

---
+++ Task Day 12/TaskDay12Solution/TaskDay12/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace TaskDay12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Problem1
+            //Func<Product, bool> Predicate01 = ProductFilter.IsOutOfStock;
+
+            //var Res0 = ListGenerators.ProductList.Where(Predicate01);
+
+            //foreach (var res in Res0)
+            //{
+            //    Console.WriteLine(res);
+            //}
+            #endregion
+        }
+    }
+
+    class ProductFilter
+    {
+        // info >> out of stock
+        public static bool IsOutOfStock(Product product)
+        {
+            return product.UnitsInStock == 0;
+        }
+        // info >> Category : Condiments
+        public static bool IsCondiments(Product product)
+        {
+            return product.Category == "Condiments";
+        }
+        public static bool IsOutOfStockAndIsCondiments(Product product)
+        {
+            return product.Category == "Condiments" && product.UnitsInStock == 0;
+        }
+        public static bool IsOutOfStockInFirstTen(Product product, int index)
+        {
+            return product.UnitsInStock == 0 && index < 10;
+        }
+    }
+}
